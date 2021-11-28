00af98d93b1e582afdac87ff727ca0ed41736d26
tashiq <fahim.csecu@gmail.com>
2021-11-28 21:03:31 +0600

    refresh problem

--- src/Pages/Login/Firebase/Firebase.config.js
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -1,9 +1,16 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyAxJCSG-QgBFZrHTBAA23IXaMOitNSTQ5M",
+  authDomain: "assignment12-d5f19.firebaseapp.com",
+  projectId: "assignment12-d5f19",
+  storageBucket: "assignment12-d5f19.appspot.com",
+  messagingSenderId: "502674374215",
+  appId: "1:502674374215:web:d7dad16a01c49861ec3b62"
+
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID
 };
 export default firebaseConfig;
